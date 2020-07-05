58c8c26782b05f332c1f05275752d073f82ff2b1
RevDonz <aprilio842@gmail.com>
2020-07-05 12:04:12 +0700

    Membuat migrations database

---
+++ database/migrations/2020_07_05_043852_create_artikel_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->bigIncrements('id_artikel');
+            $table->string('judul_artikel');
+            $table->text('isi_artikel');
+            $table->string('slug');
+            $table->string('tag');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                  ->references('id_user')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
